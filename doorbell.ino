9f12c1c8a1a980f205adbcb30bf4837ec5e52389
bee <bernhard.spindler.75@gmail.com>
2020-03-01 15:21:22 +0100

    added trigger

--- doorbell.ino
+++ doorbell.ino
@@ -2,12 +2,7 @@
 #include <WiFiClient.h>
 
 #include <Bounce2.h>
-
 #include <Homey.h>
-
-//connect one end of the doorbell push button lead to D2,
-//connect a 100K resistor from D2 to positive 3V3,
-//connect the other end of the doorbell push button lead to ground.
 
 int THEBUTTON = 4; 
 int ledState = LOW;
@@ -30,12 +25,6 @@ void setup() {
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 
-  Homey.begin("Doorbell"); // this will be the name of your button in Homey
-  Serial.println("Homey.begin()");
-  Homey.setClass("socket"); // this is the classtype. The doorbell push button behaves like a socket with on/off functionality
-  Serial.println("Homey.setClass()");
-  //Homey.addCapability("onoff", onoffCb); // adding capability _onoff to Trigger Boolean
-  Serial.println("Homey.addCapability()");
   
   //pinMode(THEBUTTON, INPUT); // declare sensor as input (i.e. the doorbell push button)
   pinMode(THEBUTTON, INPUT_PULLUP);
@@ -43,7 +32,13 @@ void setup() {
   debouncer.attach(THEBUTTON,INPUT_PULLUP); // Attach the debouncer to a pin with INPUT_PULLUP mode
   debouncer.interval(25); // Use a debounce interval of 25 milliseconds
   
-  digitalWrite(LED_BUILTIN,ledState);
+  setupHomey();
+}
+
+void setupHomey() {
+  Homey.begin("HankersMagischerKnopf");
+  //Homey.setClass("trigger");
+  Homey.trigger("pressed");
 }
 
 // the loop function runs over and over again forever
@@ -51,19 +46,11 @@ void loop() {
 
   Homey.loop();
   
-  //int doorbellState = digitalRead(THEBUTTON);
-
   debouncer.update(); // Update the Bounce instance
    
    if ( debouncer.fell() ) {  // Call code if button transitions from HIGH to LOW
      ledState = !ledState; // Toggle LED state
      digitalWrite(LED_BUILTIN,ledState); // Apply new LED state
+     Homey.trigger("pressed", (bool) (ledState ? true : false) );
    }
 }
-
-  //digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  //Serial.write("on");
-  //delay(1000);                       // wait for a second
-  //digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  //Serial.write("off");
-  //delay(1000);                       // wait for a second
